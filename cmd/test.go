5d5918a1f4ca8217b8ae423c99304ef1fee33d93
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-01-09 21:23:31 +0600

    update

--- cmd/test.go
+++ cmd/test.go
@@ -11,3 +11,14 @@ import (
 	"github.com/go-playground/form/v4"
 	"github.com/justinas/nosurf"
 )
+
+type templateData struct {
+	CurrentYear int
+	Snippet *models.Snippet
+	Snippets []*models.Snippet
+	Form any
+	Flash string
+	IsAuthenticated bool
+	CSRFToken string
+	User *models.User
+}
