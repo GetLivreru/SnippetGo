c4ac2c9ef1a576f25624ffe102cd42cbc4b99e2f
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-01-09 21:34:19 +0600

    add

--- cmd/context.go
+++ cmd/context.go
@@ -3,3 +3,10 @@ package main
 type contextKey string
 
 const isAuthenticatedContextKey = contextKey("isAuthenticated")
+
+
+const isAuthenticatedContextKey = contextKey("isAuthenticated")
+
+
+const isAuthenticatedContextKey = contextKey("isAuthenticated")
+
