702ca3a6188f401557d8e43681ca6e28caaddeb4
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-01-09 17:40:32 +0600

    main

---
+++ cmd/middleware.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"crypto/tls"
+	"database/sql"
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"snippetbox.yelnurabdrakhmanov.net/internal/models"
+
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-playground/form/v4"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+ 
