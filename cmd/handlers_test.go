63da8a14d6f9f228c40491a33d28a6790a7894ec
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-01-09 17:35:42 +0600

    fixed

--- cmd/handlers_test.go
+++ cmd/handlers_test.go
@@ -20,28 +20,6 @@ func TestPing(t *testing.T) {
 	assert.Equal(t, body, "OK")
 }
 
-package main
-
-import (
-	"net/http"
-	"net/url"
-	"testing"
-
-	"snippetbox.yelnurabdrakhmanov.net/internal/assert"
-)
-
-func TestPing(t *testing.T) {
-	app := newTestApplication(t)
-
-	ts := newTestServer(t, app.routes())
-	defer ts.Close()
-
-	code, _, body := ts.get(t, "/ping")
-
-	assert.Equal(t, code, http.StatusOK)
-	assert.Equal(t, body, "OK")
-}
-
 func TestSnippetView(t *testing.T) {
 	app := newTestApplication(t)
 	ts := newTestServer(t, app.routes())
@@ -85,7 +63,135 @@ func TestSnippetView(t *testing.T) {
 			wantCode: http.StatusNotFound,
 		},
 	}
-  
+
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+
+			assert.Equal(t, code, tt.wantCode)
+			
+			if tt.wantBody != "" {
+				assert.StringContains(t, body, tt.wantBody)
+			}
+		})
+	}
+}
+
+func TestUserSignup(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+
+	defer ts.Close()
+
+	_, _, body := ts.get(t, "/user/signup")
+	validCSRFToken := extractCSRFToken(t, body)
+
+	const (
+		validName     = "Bob"
+		validEmail    = "bob@example.com"
+		validPassword = "validPa$$word"
+		formTag       = `<form action="/user/signup" method="POST" novalidate>`
+	)
+
+	tests := []struct {
+		name           	string
+		userName 	 	string
+		userEmail 	 	string
+		userPassword 	string
+		csrfToken     	string
+		wantCode       	int
+		wantFormTag   	string
+	}{
+		{
+			name: "Valid submission",
+			userName: validName,
+			userEmail: validEmail,
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusSeeOther,
+		},
+		{
+			name: "Invalid CSRF Token",
+			userName: validName,
+			userEmail: validEmail,
+			userPassword: validPassword,
+			csrfToken: "wrongToken",
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name: "Empty name",
+			userName: "",
+			userEmail: validEmail,
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,
+		},
+		{
+			name: "Empty email",
+			userName: validName,
+			userEmail: "",
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,
+		},
+		{
+			name: "Empty password",
+			userName: validName,
+			userEmail: validEmail,
+			userPassword: "",
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,
+		},
+		{
+			name: "Invalid email",
+			userName: validName,
+			userEmail: "bob@example.",
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,
+		},
+		{
+			name: "Short password",
+			userName: validName,
+			userEmail: validEmail,
+			userPassword: "pa$$",
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,
+		},
+		{
+			name: "Duplicate email",
+			userName: validName,
+			userEmail: "dupe@example.com",
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			form := url.Values{}
+			form.Add("name", tt.userName)
+			form.Add("email", tt.userEmail)
+			form.Add("password", tt.userPassword)
+			form.Add("csrf_token", tt.csrfToken)
+			code, _, body := ts.postForm(t, "/user/signup", form)
+
+			assert.Equal(t, code, tt.wantCode)
+
+			if tt.wantFormTag != "" {
+				assert.StringContains(t, body, tt.wantFormTag)
+			}
+		})
+	}
+}
+
 func TestSnippetCreate(t *testing.T) {
 	app := newTestApplication(t)
 	ts := newTestServer(t, app.routes())
