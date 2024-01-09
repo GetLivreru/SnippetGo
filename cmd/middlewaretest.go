9ae7ad4284927be82dc466eeebf4ae7265893b06
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-01-09 19:45:55 +0600

    update

--- cmd/middlewaretest.go
+++ cmd/middlewaretest.go
@@ -27,6 +27,23 @@ func TestSecureHeaders(t *testing.T) {
 
 	rs := rr.Result()
 
+	expectedValue := "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com"
+	assert.Equal(t, rs.Header.Get("Content-Security-Policy"), expectedValue)
+
+	expectedValue = "origin-when-cross-origin"
+	assert.Equal(t, rs.Header.Get("Referrer-Policy"), expectedValue)
+
+	expectedValue = "nosniff"
+	assert.Equal(t, rs.Header.Get("X-Content-Type-Options"), expectedValue)
+	
+	expectedValue = "deny"
+	assert.Equal(t, rs.Header.Get("X-Frame-Options"), expectedValue)
+
+	expectedValue = "0"
+	assert.Equal(t, rs.Header.Get("X-XSS-Protection"), expectedValue)
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
 	defer rs.Body.Close()
 
 	body, err := io.ReadAll(rs.Body)
