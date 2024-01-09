a6d267e77cc83027938cc80faa876c2a8732b0e6
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-01-09 19:54:11 +0600

    adwdawd

--- cmd/testultest.go
+++ cmd/testultest.go
@@ -17,3 +17,15 @@ import (
 	"github.com/go-playground/form/v4"
 	"snippetbox.kaiyrbekovadilet.net/internal/models/mocks"
 )
+
+var csrfTokenRX = regexp.MustCompile(`<input type="hidden" name="csrf_token" value="(.+)" />`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+	matches := csrfTokenRX.FindStringSubmatch(body)
+
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(string(matches[1]))
+}
