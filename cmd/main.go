c2ece3a24f93b61a623ea03bf5157dd6d46c7cb3
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-01-09 19:49:28 +0600

    addd

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 	"os"
 	"time"
 
-	"snippetbox.yelnurabdrakhmanov.net/internal/models"
+	"snippetbox.kaiyrbekovadilet.net/internal/models"
 
 	"github.com/alexedwards/scs/mysqlstore"
 	"github.com/alexedwards/scs/v2"
