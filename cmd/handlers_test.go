7403d16e655a9b252e781e2d256e377e17010312
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-01-09 17:35:00 +0600

    fix3ed

--- cmd/handlers_test.go
+++ cmd/handlers_test.go
@@ -20,4 +20,97 @@ func TestPing(t *testing.T) {
 	assert.Equal(t, body, "OK")
 }
 
- 
+package main
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+
+	"snippetbox.yelnurabdrakhmanov.net/internal/assert"
+)
+
+func TestPing(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, body := ts.get(t, "/ping")
+
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
+}
+
+func TestSnippetView(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	test := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name: "Valid ID",
+			urlPath: "/snippet/view/1",
+			wantCode: http.StatusOK,
+			wantBody: "An old silent pond...",
+		},
+		{
+			name: "Non-existent ID",
+			urlPath: "/snippet/view/2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name: "Negative ID",
+			urlPath: "/snippet/view/-1",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name: "Decimal ID",
+			urlPath: "/snippet/view/1.23",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name: "String ID",
+			urlPath: "/snippet/view/foo",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name: "Empty ID",
+			urlPath: "/snippet/view/",
+			wantCode: http.StatusNotFound,
+		},
+	}
+  
+func TestSnippetCreate(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/snippet/create")
+
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, headers.Get("Location"), "/user/login")
+	})
+
+	t.Run("Authenticated", func(t *testing.T) {
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+
+		form := url.Values{}
+		form.Add("email", "elnur.kazakhstan@gmail.com")
+		form.Add("password", "elnur2005")
+		form.Add("csrf_token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+
+		code, _, body := ts.get(t, "/snippet/create")
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, `<form action="/snippet/create" method="POST">`)
+	})
+}
+	
